31abe0c293bd1547d1b5d47871811b526e6289a8
Chris Calo <chris.calo@gmail.com>
2019-09-30 23:24:39 -0400

    Try adding click handlers

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,15 @@ export default {
         <a href="/about/">About</a>
       </nav>
       <h1>Hello, {{ name }}!</h1>
+      <button @click="counter--">−</button>
+      <span>{{ counter }}</span>
+      <button @click="counter++">+</button>
     </div>
   `,
   data() {
     return {
       name: "World",
+      counter: 0,
     };
   },
 };
