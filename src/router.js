b29ef87bd3a1f0339f6744bf5873159342b8eb18
Chris Calo <ccalo@google.com>
2019-10-05 12:41:28 -0400

    Formatting

--- src/router.js
+++ src/router.js
@@ -17,9 +17,6 @@ export function createRouter() {
       {
         path: "/about/",
         name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () =>
           import(/* webpackChunkName: "about" */ "./views/about/index.vue")
       }
