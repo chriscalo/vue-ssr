f135025c1e763edf9ef3d787bb6c6ff36f4b9570
Chris Calo <ccalo@google.com>
2019-10-05 12:16:13 -0400

    Move nav styles to Nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="nav">
+  <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </nav>
 </template>
 
 <script>
@@ -10,5 +10,15 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+  nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
 </style>
