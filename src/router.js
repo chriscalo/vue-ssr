5ac6499dee50efbf75290250380cafa145b4a1fb
Chris Calo <ccalo@google.com>
2019-10-05 12:24:59 -0400

    Trailing slash for about page

--- src/router.js
+++ src/router.js
@@ -16,12 +16,12 @@ export function createRouter () {
       component: index
     },
     {
-      path: '/about',
+      path: '/about/',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/about.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/about/index.vue')
     }
   ]
 })
