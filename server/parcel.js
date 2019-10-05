f4e718fd19e338b6399cc4f5261c3be02a6664ac
Chris Calo <chris.calo@gmail.com>
2019-10-05 01:07:19 -0400

    Add client bundler

--- server/parcel.js
+++ server/parcel.js
@@ -7,13 +7,14 @@ import { render } from "./vue-ssr";
 export const server = express();
 export default server;
 
-const outDir = path.resolve(__dirname, "../.parcel");
+const serverBuildDir = path.resolve(__dirname, "../.parcel/server");
+const clientBuildDir = path.resolve(__dirname, "../.parcel/client");
 const entryFiles = "./pages/**/*.vue";
 
-const options = {
+const serverOptions = {
   // The out directory to put the build files in
   // defaults to dist
-  outDir,
+  outDir: serverBuildDir,
   // Whether to watch the files and rebuild them on change
   // defaults to process.env.NODE_ENV !== 'production'
   watch: true,
@@ -27,7 +28,27 @@ const options = {
   autoInstall: true,
 };
 
-new Bundler(entryFiles, options).bundle();
+const clientOptions = {
+  // The out directory to put the build files in
+  // defaults to dist
+  outDir: clientBuildDir,
+  // Whether to watch the files and rebuild them on change
+  // defaults to process.env.NODE_ENV !== 'production'
+  watch: true,
+  // Browser/node/electron
+  // defaults to browser
+  target: "browser",
+  // Enable or disable HMR while watching
+  hmr: true,
+  // Enable or disable auto install of missing dependencies found during
+  // bundling
+  autoInstall: true,
+};
+
+new Bundler(entryFiles, serverOptions).bundle();
+new Bundler(entryFiles, clientOptions).bundle();
+
+server.use(express.static(clientBuildDir));
 
 server.use(async (req, res, next) => {
   const { url } = req;
@@ -44,7 +65,7 @@ server.use(async (req, res, next) => {
   }
 });
 
-function html({ body = "", title = "" }) {
+function html({ body = "", title = "",  }) {
   return `
     <!DOCTYPE html>
     <html lang="en">
