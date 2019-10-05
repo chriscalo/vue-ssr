3cc424733c185fd2be50ef46be1d2224f9ac3f9e
Chris Calo <ccalo@google.com>
2019-10-05 11:36:58 -0400

    meh, sorta get parcel HTML entry point working

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Vue from "vue/dist/vue.esm.js";
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      name: "World",
+      counter: 0,
+    };
+  },
+});
--- server/parcel.js
+++ server/parcel.js
@@ -7,31 +7,13 @@ import { render } from "./vue-ssr";
 export const server = express();
 export default server;
 
-const serverBuildDir = path.resolve(__dirname, "../.parcel/server");
-const clientBuildDir = path.resolve(__dirname, "../.parcel/client");
-const entryFiles = "./pages/**/*.vue";
+const buildDir = path.resolve(__dirname, "../.parcel");
+const entryFiles = "./pages/**/*.html";
 
-const serverOptions = {
+const options = {
   // The out directory to put the build files in
   // defaults to dist
-  outDir: serverBuildDir,
-  // Whether to watch the files and rebuild them on change
-  // defaults to process.env.NODE_ENV !== 'production'
-  watch: true,
-  // Browser/node/electron
-  // defaults to browser
-  target: "node",
-  // Enable or disable HMR while watching
-  hmr: true,
-  // Enable or disable auto install of missing dependencies found during
-  // bundling
-  autoInstall: true,
-};
-
-const clientOptions = {
-  // The out directory to put the build files in
-  // defaults to dist
-  outDir: clientBuildDir,
+  outDir: buildDir,
   // Whether to watch the files and rebuild them on change
   // defaults to process.env.NODE_ENV !== 'production'
   watch: true,
@@ -45,36 +27,21 @@ const clientOptions = {
   autoInstall: true,
 };
 
-new Bundler(entryFiles, serverOptions).bundle();
-new Bundler(entryFiles, clientOptions).bundle();
+new Bundler(entryFiles, options).bundle();
 
-server.use(express.static(clientBuildDir));
+server.use(express.static(buildDir));
 
-server.use(async (req, res, next) => {
-  const { url } = req;
-  const bundlePath = path.join(outDir, url, "index.js");
-  
-  try {
-    const { default: component } = await import(bundlePath);
-    const content = html({
-      body: await render(component),
-    });
-    res.send(content);
-  } catch (error) {
-    next();
-  }
-});
-
-function html({ body = "", title = "",  }) {
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8"/>
-        <title>${ title }</title>
-        <link href="/styles.css" rel="stylesheet"/>
-      </head>
-      <body>${ body }</body>
-    </html>
-  `;
-}
+// server.use(async (req, res, next) => {
+//   const { url } = req;
+//   const bundlePath = path.join(outDir, url, "index.js");
+//
+//   try {
+//     const { default: component } = await import(bundlePath);
+//     const content = html({
+//       body: await render(component),
+//     });
+//     res.send(content);
+//   } catch (error) {
+//     next();
+//   }
+// });
